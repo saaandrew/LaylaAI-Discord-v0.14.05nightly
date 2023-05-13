28c21b2b19dbca9a83e448e7f5745f0d58d5d50e
QuynhNhuCoding.cpp <86618490+saaandrew@users.noreply.github.com>
2023-05-13 23:34:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -70,88 +70,107 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
-@bot.command()
+@bot.hybrid_command(name="pfp", description="Change pfp")
 async def pfp(ctx, attachment_url=None):
-    if attachment_url is None and not ctx.message.attachments:
-        return await ctx.send(
-            "Please provide an image URL or attach an image with the command"
-        )
+  if attachment_url is None and not ctx.message.attachments:
+    return await ctx.send(
+      "Please provide an image URL or attach an image with the command")
+  if attachment_url is None:
+    attachment_url = ctx.message.attachments[0].url
+  async with aiohttp.ClientSession() as session:
+    async with session.get(attachment_url) as response:
+      await bot.user.edit(avatar=await response.read())
 
-    if attachment_url is None:
-        attachment_url = ctx.message.attachments[0].url
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(attachment_url) as response:
-            await bot.user.edit(avatar=await response.read())
-
-    await ctx.send("My profile picture has been updated!")
-
-@bot.command()
+@bot.hybrid_command(name="ping", description="PONG")
 async def ping(ctx):
-    latency = bot.latency * 1000
-    await ctx.send(f"Pong! Latency: {latency:.2f} ms")
+  latency = bot.latency * 1000
+  await ctx.send(f"Pong! Latency: {latency:.2f} ms")
 
-@bot.command()
+
+@bot.hybrid_command(name="changeusr",
+                    description="Change bot's actual username")
 async def changeusr(ctx, new_username):
-    # Check that the new username is not already taken
-    taken_usernames = [user.name.lower() for user in bot.get_all_members()]
-    if new_username.lower() in taken_usernames:
-        await ctx.send(f"Sorry, the username '{new_username}' is already taken.")
-        return
+  taken_usernames = [user.name.lower() for user in bot.get_all_members()]
+  if new_username.lower() in taken_usernames:
+    await ctx.send(f"Sorry, the username '{new_username}' is already taken.")
+    return
+  if new_username == "":
+    await ctx.send("Please send the new username as well!")
+    return
+  try:
+    await bot.user.edit(username=new_username)
+  except discord.errors.HTTPException as e:
+    await ctx.send("".join(e.text.split(":")[1:]))
 
-@bot.command()
-@commands.has_permissions(administrator=True)
+
+@bot.hybrid_command(name="toggledm", description="Toggle dm for chatting")
 async def toggledm(ctx):
-    global allow_dm
-    allow_dm = not allow_dm
-    await ctx.send(f"DMs are now {'allowed' if allow_dm else 'disallowed'} for active channels.")
-    
-@bot.command()
-@commands.has_permissions(administrator=True)
+  global allow_dm
+  allow_dm = not allow_dm
+  await ctx.send(
+    f"DMs are now {'allowed' if allow_dm else 'disallowed'} for active channels."
+  )
+
+
+@bot.hybrid_command(name="toggleactive", description="Toggle active channels")
 async def toggleactive(ctx):
-    channel_id = ctx.channel.id
-    if channel_id in active_channels:
-        active_channels.remove(channel_id)
-        with open("channels.txt", "w") as f:
-            for id in active_channels:
-                f.write(str(id) + "\n")
-        await ctx.send(
-            f"{ctx.channel.mention} has been removed from the list of active channels."
-        )
-    else:
-        active_channels.add(channel_id)
-        with open("channels.txt", "a") as f:
-            f.write(str(channel_id) + "\n")
-        await ctx.send(
-            f"{ctx.channel.mention} has been added to the list of active channels.")
+  channel_id = ctx.channel.id
+  if channel_id in active_channels:
+    active_channels.remove(channel_id)
+    with open("channels.txt", "w") as f:
+      for id in active_channels:
+        f.write(str(id) + "\n")
+    await ctx.send(
+      f"{ctx.channel.mention} has been removed from the list of active channels."
+    )
+  else:
+    active_channels.add(channel_id)
+    with open("channels.txt", "a") as f:
+      f.write(str(channel_id) + "\n")
+    await ctx.send(
+      f"{ctx.channel.mention} has been added to the list of active channels.")
+
 
 # Read the active channels from channels.txt on startup
 if os.path.exists("channels.txt"):
-    with open("channels.txt", "r") as f:
-        for line in f:
-            channel_id = int(line.strip())
-            active_channels.add(channel_id)
-      
-@bot.command(name='bonk')
-async def _bonk(ctx):
-    bonk()
-    await ctx.send('What are we talking about, Rick Astley?')
+  with open("channels.txt", "r") as f:
+    for line in f:
+      channel_id = int(line.strip())
+      active_channels.add(channel_id)
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You don't have administrator permissions to use this command. Please contact server owner.")
 
-@bot.command()
-async def welp(ctx):
-    embed = discord.Embed(title="Bot Commands", color=0x00ff00)
-    embed.add_field(name="!pfp [image_url]", value="Change the bot's profile picture", inline=False)
-    embed.add_field(name="!bonk", value="Clears history of the bot", inline=False)
-    embed.add_field(name="!changeusr [new_username]", value="Change the bot's username", inline=False)
-    embed.add_field(name="!ping", value="Pong", inline=False)
-    embed.add_field(name="!toggleactive", value="Toggle the current channel to the list of active channels", inline=False)   
-    embed.add_field(name="!toggledm", value="Toggle if DM should be active or not", inline=False)   
-    embed.set_footer(text="saaandrew da best!!")
+@bot.hybrid_command(name="bonk", description="Clear bot's memory")
+async def bonk(ctx):
+  global message_history
+  message_history.clear()
+  await ctx.send("What did you just say? Rick Astley?")
+
+
+bot.remove_command("help")
+
+
+@bot.hybrid_command(name="help", description="Get all other commands!")
+async def help(ctx):
+  embed = discord.Embed(title="Bot Commands", color=0x00ff00)
+  embed.add_field(name="!pfp [image_url]",
+                  value="Change the bot's profile picture",
+                  inline=False)
+  embed.add_field(name="!bonk",
+                  value="Clears history of the bot",
+                  inline=False)
+  embed.add_field(name="!changeusr [new_username]",
+                  value="Change the bot's username",
+                  inline=False)
+  embed.add_field(name="!ping", value="Pong", inline=False)
+  embed.add_field(
+    name="!toggleactive",
+    value="Toggle the current channel to the list of active channels",
+    inline=False)
+  embed.add_field(name="!toggledm",
+                  value="Toggle if DM should be active or not",
+                  inline=False)
+  embed.set_footer(text="saaandrew is da best ^vvvv^")
             
 
 keep_alive()
